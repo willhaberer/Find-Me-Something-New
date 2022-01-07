050e5346cbfd97c8ad1b4a10a3cec3c95235f7c7
Will Haberer <willhaberer@gmail.com>
2022-01-07 11:53:37 -0500

    added navbar links to future pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
       <h1>Welcome to Find Me Something New</h1>
+      <ul id="navtabs">
+        <Link id="link" style={{ textDecoration: "none" }} to="/spotify">
+          <li>Spotify</li>
+        </Link>
+        <Link id="link" style={{ textDecoration: "none" }} to="/youtube">
+          <li>Youtube</li>
+        </Link>
+        <Link id="link" style={{ textDecoration: "none" }} to="/soundcloud">
+          <li>Soundcloud</li>
+        </Link>
+      </ul>
       <h2>Please choose one of the above sections to get started!</h2>
     </div>
   );
