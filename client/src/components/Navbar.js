a1638ba46c6a8b64d622e12a0a2be47d2a527d1f
Will Haberer <willhaberer@gmail.com>
2022-01-07 11:40:02 -0500

    added Navbar.js to components

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav id="navbar">
+      <h1>FMSN</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
