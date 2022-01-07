1ee5668a3c393d06baa616d05d00fcf72446b855
Will Haberer <willhaberer@gmail.com>
2022-01-07 12:12:56 -0500

    selecting FMSN now takes user back to home page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav id="navbar">
-      <h1>FMSN</h1>
+    <nav>
+      <Link id="link" style={{ textDecoration: "none" }} to="/">
+        <li>FMSN</li>
+      </Link>
     </nav>
   );
 };
