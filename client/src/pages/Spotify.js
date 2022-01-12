df54623d4db256c01c5ba4891e443fcf7eab9ece
Will Haberer <willhaberer@gmail.com>
2022-01-12 16:09:46 -0500

    useState implemented in spotify page to dynamically update the spotify player

--- client/src/pages/Spotify.js
+++ client/src/pages/Spotify.js
@@ -3,6 +3,13 @@ import { getSpotifyToken, getRecTrack } from "../utils/API";
 import { useMutation } from "@apollo/react-hooks";
 
 function Spotify() {
+  const [embedCode, setEmbedCode] = useState(
+    "https://open.spotify.com/embed/track/5bwSx1hwmUivlUgY9UhfEB"
+  );
+  // const embedCode = {
+  //   trackID: "https://open.spotify.com/embed/track/5bwSx1hwmUivlUgY9UhfEB",
+  // };
+
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -19,7 +26,10 @@ function Spotify() {
 
       const result = await getRecTrack(token);
       const trackData = await result.json();
-      console.log(trackData);
+      const trackID = trackData.tracks[0].id;
+      console.log(trackID);
+      setEmbedCode("https://open.spotify.com/embed/track/" + trackID);
+      console.log(embedCode);
     } catch (err) {
       console.error(err);
     }
@@ -29,8 +39,27 @@ function Spotify() {
     <div>
       <h1>this is the spotify page</h1>
       <button onClick={handleFormSubmit}>Get Song</button>
+      <iframe
+        src={embedCode}
+        width="300"
+        height="380"
+        frameBorder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
+      ></iframe>
     </div>
   );
+}
+
+{
+  /* <iframe
+        src={embedCode.name}
+        width="300"
+        height="380"
+        frameborder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
+      ></iframe> */
 }
 
 export default Spotify;
