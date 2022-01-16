e11f46e0d8f1bd04d364d9b9b49db37fe8142483
Will Haberer <willhaberer@gmail.com>
2022-01-16 12:07:05 -0500

    setup youtube page to handle an embedded video, set state to generic vid for now

--- client/src/pages/Youtube.js
+++ client/src/pages/Youtube.js
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { getYoutubeVid, getVidId } from "../utils/API";
-import { useMutation } from "@apollo/react-hooks";
 
 function Youtube() {
-  // create method to search for books and set state on form submit
+  const [embedCode, setEmbedCode] = useState("initial");
+
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
     try {
+      setEmbedCode("https://www.youtube.com/embed/PcSPfzU1Wyk");
+
       const response = await getVidId();
 
       if (!response) {
@@ -22,12 +24,32 @@ function Youtube() {
     }
   };
 
-  return (
-    <div>
-      <h1>this is the youtube page</h1>
-      <button onClick={handleFormSubmit}>Get Youtube Vid</button>
-    </div>
-  );
+  if (embedCode === "initial") {
+    return (
+      <div id="container">
+        <button id="vidBtn" className="bouncy" onClick={handleFormSubmit}>
+          Get Vid
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div id="container">
+        <button id="vidBtn" className="bouncy" onClick={handleFormSubmit}>
+          Get Vid
+        </button>
+        <iframe
+          width="560"
+          height="315"
+          src={embedCode}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    );
+  }
 }
 
 export default Youtube;
