af25ab4907150199073e50aff696e9c0b0ca74f2
Will Haberer <willhaberer@gmail.com>
2022-01-16 12:14:28 -0500

    updated soundcloud page with coming soon text

--- client/src/pages/Soundcloud.js
+++ client/src/pages/Soundcloud.js
@@ -3,7 +3,11 @@ import React from "react";
 function Soundcloud() {
   return (
     <div>
-      <h1>this is the soundcloud page</h1>
+      <h1>Coming Soon!</h1>
+      <h2>
+        Soundcloud is currently not accepting new projects to be built off its
+        API but once they open back up this page will be updated
+      </h2>
     </div>
   );
 }
