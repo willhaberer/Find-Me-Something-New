eb336252eebe9ec1733a54454f301e703c1f476d
Will Haberer <willhaberer@gmail.com>
2022-01-19 20:29:48 -0500

    navbar does not display login or signup if user data exists

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import { GET_ME } from "../utils/queries";
 import "../styles/Navbar.css";
 import logo from "../assets/fmsnLogoWhite.png";
 
 const Navbar = () => {
+  const { loading, data } = useQuery(GET_ME);
+  const userData = data?.me || {};
+
+  if (!userData?.username) {
+    return (
+      <div id="navbar">
+        <Link id="link" style={{ textDecoration: "none" }} to="/">
+          <img
+            id="logo"
+            className="image"
+            src={logo}
+            alt="logo"
+            height="15%"
+            width="15%"
+          />
+        </Link>
+        <Link id="link" to="/login">
+          <h3 id="loginLink">Login</h3>
+        </Link>
+        <Link id="link" to="/about">
+          <h3 id="aboutLink">About</h3>
+        </Link>
+        <Link id="link" to="/signup">
+          <h3 id="signupLink">Signup</h3>
+        </Link>
+        <Link id="link" to="/profile">
+          <h3 id="profileLink">Profile</h3>
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <div id="navbar">
       <Link id="link" style={{ textDecoration: "none" }} to="/">
@@ -16,14 +50,8 @@ const Navbar = () => {
           width="15%"
         />
       </Link>
-      <Link id="link" to="/login">
-        <h3 id="loginLink">Login</h3>
-      </Link>
       <Link id="link" to="/about">
         <h3 id="aboutLink">About</h3>
-      </Link>
-      <Link id="link" to="/signup">
-        <h3 id="signupLink">Signup</h3>
       </Link>
       <Link id="link" to="/profile">
         <h3 id="profileLink">Profile</h3>
