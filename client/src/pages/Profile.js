f9b4418cfb136b0f9d5d66dab6c2e96728bcf036
Will Haberer <willhaberer@gmail.com>
2022-01-19 19:59:51 -0500

    created profile page with imports

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { useQuery, useMutation } from "@apollo/react-hooks";
+// import { getMe, deleteBook } from "../utils/API";
+import { GET_ME } from "../utils/queries";
+
+import Auth from "../utils/auth";
