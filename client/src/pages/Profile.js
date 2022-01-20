b7cde0415720a9988d30137cba50705348a452da
Will Haberer <willhaberer@gmail.com>
2022-01-19 20:01:32 -0500

    added conditional return to profile page checking for user data

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,3 +5,17 @@ import { useQuery, useMutation } from "@apollo/react-hooks";
 import { GET_ME } from "../utils/queries";
 
 import Auth from "../utils/auth";
+
+const Profile = () => {
+  if (!userData?.username) {
+    return (
+      <h4>
+        You must be logged in to view this page! Please log in and try again!
+      </h4>
+    );
+  }
+
+  return <h4>somehow the conditional failed</h4>;
+};
+
+export default Profile;
