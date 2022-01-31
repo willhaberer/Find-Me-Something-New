bc4b1348173c3b5ca6c83087693791b112c401bd
Will Haberer <willhaberer@gmail.com>
2022-01-31 18:08:51 -0500

    removed env variable for mongoose.connect

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fmsn", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  "mongodb+srv://fmsn:oqlIcIACAQrIh85G@cluster0.lq4uh.mongodb.net/fmsnData?retryWrites=true&w=majority" ||
+    "mongodb://localhost/fmsn",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
